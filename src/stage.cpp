df16c50a880698ca5092889f60ebcbda93d1adc7
MarcGutierrez <72510629+MarcGutierrez@users.noreply.github.com>
2023-06-27 19:06:52 +0200

    put again random enemy num

--- src/stage.cpp
+++ src/stage.cpp
@@ -113,7 +113,7 @@ stageId TitleStage::getId()
 
 void PlayStage::loadNewLvl()
 {
-    int enemyNum = 1;//get_random_enemy_num(currentDiff);
+    int enemyNum = get_random_enemy_num(currentDiff);
     // this code is for if we want to use it to change things via randomness or other factors like difficulty and position and to not destroy enemies on death
     for (int i = 0; i < enemyNum; i++)
     {
